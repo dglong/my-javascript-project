29deac64ea8506dae5095d637a8574172198a64b
dglong <long.danghoang@proton.me>
2022-05-03 03:47:04 +0700

    Fix styling

--- projects/project-todo-list/index.js
+++ projects/project-todo-list/index.js
@@ -1,7 +1,10 @@
 const todoInput = document.getElementById("todo-input");
+const todoList = document.getElementById("todo-list");
 
 function newTodo() {
-  let todoInputValue = todoInput.value;
-  console.log(todoInputValue);
-  todoInput.value = "";
+  console.log(todoList);
+  const listItem = document.createElement("li");
+  listItem.classList.add("todo-item");
+  listItem.innerText = todoInput.value;
+  todoList.appendChild(listItem);
 }
